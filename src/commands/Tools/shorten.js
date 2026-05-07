34050882771006893eb0afdd6c4a36a14d41c07d
Aniket-soam-star <renucoil271@gmail.com>
2026-05-07 10:50:47 +0530

    Update User-Agent header in shorten.js

--- src/commands/Tools/shorten.js
+++ src/commands/Tools/shorten.js
@@ -72,7 +72,7 @@ export default {
                 response = await fetch(apiUrl, {
                     signal: controller.signal,
                     headers: {
-                        'User-Agent': 'TitanBot URL Shortener/1.0'
+                        'User-Agent': 'ZeroBot/1.0'
                     }
                 });
             } catch (networkError) {
@@ -134,5 +134,4 @@ export default {
         }
     },
 };
-
 
